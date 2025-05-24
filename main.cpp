408c3f2310b54b1fbee987a20c45157f481e7da6
Nikolai <nikolaidahljensen@gmail.com>
2025-05-24 20:07:17 +0200

    Åbner og lukker database når program kører

--- main.cpp
+++ main.cpp
@@ -3,10 +3,21 @@
 #include "Game.h"
 #include "Fight.h"
 #include <iostream>
+#include <sqlite3.h>
 
 int main()
 {
-	Game game;
-	game.start();
-	return 0;
+    sqlite3* db;
+    int rc = sqlite3_open("../myDataBase.db", &db);  // åbner (eller opretter) databasefilen "game.db"
+
+    if (rc) {
+        std::cerr << "Can't open database: " << sqlite3_errmsg(db) << std::endl;
+        return 1;
+    }
+
+    Game game;
+    game.start(db);  // kalder start med db
+
+    sqlite3_close(db);  // luk databasen, når programmet slutter
+    return 0;
 }
